95f29a92fd3a53bba0414689e41ac8e5fa6789e1
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-06 00:28:33 +0500

    App js is ready for use

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import { useLocation, useRoutes } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import cookies from "js-cookie";
+import languages from "./i-18/resources";
+import AdminDashboardLayout from "./layouts/AdminDashboardLayout";
+import AppLayout from "./layouts/AppLayout";
+import UserDashboardLayout from "./layouts/UserDashboardLayout";
+import routes from "./routes/appRoutes";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  const content = useRoutes(routes);
+  const currentLanguageCode = cookies.get("i18next") || "uz";
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
+  const { t } = useTranslation();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || "ltr";
+    document.title = t("app_title");
+  }, [currentLanguage, t]);
+
+  if (pathname.includes("/admin/dashboard")) {
+    return <AdminDashboardLayout>{content}</AdminDashboardLayout>;
+  }
+
+  if (pathname.includes("/user/dashboard")) {
+    return <UserDashboardLayout>{content}</UserDashboardLayout>;
+  }
+  return <AppLayout>{content}</AppLayout>;
 }
 
 export default App;
---
+++ src/layouts/UserDashboardLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserDashboardLayout = () => {
+  return <div>UserDashboardLayout</div>;
+};
+
+export default UserDashboardLayout;
