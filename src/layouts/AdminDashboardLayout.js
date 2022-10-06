392d977b078abd63d360697fca89c7a2ee6518b5
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-07 00:23:39 +0500

    Admin and Moderator dashboard layout completed

--- src/layouts/AdminDashboardLayout.js
+++ src/layouts/AdminDashboardLayout.js
@@ -1,7 +1,34 @@
-import React from "react";
+import { Fragment, useState } from "react";
+import DashboardNavbar from "./admin/DashboardNavbar";
+import DashboardSidebar from "./admin/DashboardSidebar";
+import { BodyWrapper, InnerWrapper } from "./styled/Components";
 
-const AdminDashboardLayout = () => {
-  return <div>AdminDashboardLayout</div>;
+const AdminDashboardLayout = ({ children }) => {
+  const [sidebarCompact, setSidebarCompact] = useState(false);
+  const [showMobileSideBar, setShowMobileSideBar] = useState(false);
+  // handle sidebar toggle for desktop device
+
+  const handleCompactToggle = () => setSidebarCompact((state) => !state);
+  // handle sidebar toggle in mobile device
+
+  const handleMobileDrawerToggle = () =>
+    setShowMobileSideBar((state) => !state);
+
+  return (
+    <Fragment>
+      <DashboardSidebar
+        sidebarCompact={sidebarCompact}
+        showMobileSideBar={showMobileSideBar}
+        setSidebarCompact={handleCompactToggle}
+        setShowMobileSideBar={handleMobileDrawerToggle}
+      />
+
+      <BodyWrapper compact={sidebarCompact ? 1 : 0}>
+        <DashboardNavbar handleDrawerToggle={handleMobileDrawerToggle} />
+        <InnerWrapper>{children}</InnerWrapper>
+      </BodyWrapper>
+    </Fragment>
+  );
 };
 
 export default AdminDashboardLayout;
