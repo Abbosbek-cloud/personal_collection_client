25e17ede7f083c0d7d03ca15356b30a9c2b6a4d0
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-16 15:15:01 +0500

    file extions changed

---
+++ src/pages/PerCollection.jsx
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const PerCollection = () => {
+  return (
+    <Container>
+      <Box></Box>
+    </Container>
+  );
+};
+
+export default PerCollection;
