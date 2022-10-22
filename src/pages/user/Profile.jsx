db72ce91f3da06889a1654c5bd50063604ce7da5
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-22 12:06:23 +0500

    user profile page started

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import UserSections from "../../components/UserSections";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ handleDrawer }) => {
+  const { t } = useTranslation();
+
+  return <UserSections header={t("profile")} />;
 };
 
 export default Profile;
