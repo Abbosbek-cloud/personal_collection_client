4e7cedeed0f3eceb38a13d8656fa2008909f5bfa
Abbosbek <abek01sulaymonov@gmail.com>
2022-10-24 15:15:25 +0300

    react-quill added

--- src/pages/user/PerItem.jsx
+++ src/pages/user/PerItem.jsx
@@ -1,7 +1,22 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import ReactQuill from "../../components/ReactQuill";
 
 const PerItem = () => {
-  return <div>PerItem</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <ReactQuill
+          name="description"
+          placeholder="Mahsulot haqida"
+          theme="snow"
+          value={values.description}
+          onChange={(data) => setFieldValue("description", data)}
+          sx={{ height: 400 }}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default PerItem;
