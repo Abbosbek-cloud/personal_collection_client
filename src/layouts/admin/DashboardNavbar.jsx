b2d4f2baaef66c354267ecbc89b1fb3ae2e1807c
Abbosbek <abek01sulaymonov@gmail.com>
2022-11-01 15:25:23 +0300

    popover added

--- src/layouts/admin/DashboardNavbar.jsx
+++ src/layouts/admin/DashboardNavbar.jsx
@@ -14,6 +14,7 @@ import {
 import AccountPopover from "./popovers/AccountPopover";
 import NotificationsPopover from "./popovers/NoficationPopover";
 import { useNavigate } from "react-router-dom";
+import ChangeLanguage from "../../components/ChangeLanguage";
 
 const DashboardNavbar = ({ handleDrawerToggle }) => {
   const navigate = useNavigate();
@@ -57,7 +58,7 @@ const DashboardNavbar = ({ handleDrawerToggle }) => {
               }
             />
 
-            <NotificationsPopover />
+            <ChangeLanguage />
             <AccountPopover />
           </FlexBox>
         </StyledToolBar>
